63e8ba55ff19112b684e88cc4c33383ab5941d3a
Cal <sakphea@gmail.com>
2025-10-10 12:41:11 +0700

    Implemented file system logic into main

--- src/main.c
+++ src/main.c
@@ -9,29 +9,43 @@ int main(void)
     init_ui();
     init_keys();
 
-    int selected = 0;
+    FileEntry entry = {0};
+    list_dir(".", &entry);
 
+    int cursor_index = 0;
     int ch;
     while ((ch = getch()) != 'q')
     {
         mvprintw(0, 0, "Press up or down arrow key to move, q to quit.\n\n");
 
-        list_dir(".");
+        for (int i = 0; i < entry.item_count; i++)
+        {
+            if (cursor_index == i)
+            {
+                attron(A_REVERSE);
+            }
+            mvprintw(i + 2, 2, "%s", entry.item_entry[i]);
+            attroff(A_REVERSE);
+        }
 
         Action action = get_action(ch);
         switch (action)
         {
             case MOVE_UP:
-                selected--;
-                if (selected < 0)
+                if (cursor_index > 0)
                 {
-                    selected = 0;
+                    cursor_index--;
                 }
                 break;
             case MOVE_DOWN:
-                selected++;
+                if (cursor_index < entry.item_count - 1)
+                {
+                    cursor_index++;
+                }
                 break;
         }
+        mvprintw(15, 0, "%i", cursor_index);
+        mvprintw(16, 0, "%i", entry.item_count);
     }
 
     clean_ui();
