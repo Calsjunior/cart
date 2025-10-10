01c916abc570d74b2161f2e798bf628fa56c51a3
Cal <sakphea@gmail.com>
2025-10-10 12:40:08 +0700

    Added a struct FileEntry
    struct used for counting items inside the directory and passing them
    into the array

--- include/fs.h
+++ include/fs.h
@@ -1,6 +1,14 @@
 #ifndef FS_H
 #define FS_H
 
-void list_dir(const char *dirname);
+#define MAX_ITEMS 256
+
+typedef struct
+{
+    char *item_entry[MAX_ITEMS];
+    int item_count;
+} FileEntry;
+
+void list_dir(const char *dirname, FileEntry *entry);
 
 #endif
