ef20bd2142f947757382bab454548c1ffa4add41
cal <sakphea05@gmail.com>
2025-11-05 23:04:27 +0700

    Added delete UI

---
+++ include/components/delete.h
@@ -0,0 +1,11 @@
+#ifndef DELETE_H
+#define DELETE_H
+
+#include "components/modal.h"
+#include "fs.h"
+#include "state.h"
+#include "text_utils.h"
+
+void draw_delete_entry_prompt(AppState *state, EntryList *list);
+
+#endif
---
+++ src/components/delete.c
@@ -0,0 +1,43 @@
+#include "components/delete.h"
+
+void draw_delete_entry_prompt(AppState *state, EntryList *list)
+{
+    ModalConfig config = {
+        .title = "Delete this entry?",
+        .min_height = 7,
+        .min_width = 45,
+        .height_ratio = 0.2,
+        .width_ratio = 0.3,
+        .alignment = MODAL_TOPHALF_CENTER,
+    };
+
+    int height, width;
+    WINDOW *delete_win = create_modal(config, &height, &width);
+
+    char full_path[PATH_MAX];
+    helper_set_full_path(full_path, sizeof(full_path), state->dir_path, list->cursor->name);
+
+    char display_path[512];
+    int max_path_width = width - 6;
+    truncate_path(display_path, sizeof(display_path), full_path, max_path_width);
+
+    // Prints the path name
+    int path_y = 3;
+    int path_x = height / 2;
+    mvwprintw(delete_win, path_y, path_x, "%s", display_path);
+
+    // Prints yes/no option
+    const char *yes_option = "[Y]es";
+    const char *no_option = "[N]o";
+
+    int spacing = 15;
+    int total_width = strlen(yes_option) + strlen(no_option) + spacing;
+    int option_y = 6;
+    int option_x = (width - total_width) / 2;
+
+    mvwprintw(delete_win, option_y, option_x, " %s ", yes_option);
+    mvwprintw(delete_win, option_y, option_x + strlen(yes_option) + spacing, " %s ", no_option);
+
+    wrefresh(delete_win);
+    destroy_modal(delete_win);
+}
